e3bdbdfcc30c6b4b8790525eb497cc08d00ccafb
ddintrono1 <d.dintrono1@studenti.poliba.it>
2025-03-26 17:51:15 +0100

    delete first version of the project
    a former version of the project has been deleted

--- Project-control-stepper/Debug/Core/Src/vl53l0x_platform_log.d
+++
@@ -1,7 +0,0 @@
-Core/Src/vl53l0x_platform_log.o: ../Core/Src/vl53l0x_platform_log.c \
- ../Core/Inc/vl53l0x_def.h ../Core/Inc/vl53l0x_device.h \
- ../Core/Inc/vl53l0x_types.h ../Core/Inc/vl53l0x_platform_log.h
-../Core/Inc/vl53l0x_def.h:
-../Core/Inc/vl53l0x_device.h:
-../Core/Inc/vl53l0x_types.h:
-../Core/Inc/vl53l0x_platform_log.h:
